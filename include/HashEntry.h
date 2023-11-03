7c401921f6118312754dd16fb20e9bb0a130f304
Federico Cabanilla <federico.cabanilla@gmail.com>
2023-11-02 22:53:26 -0300

    Updated HashMap.h HashEntry.h

--- include/HashEntry.h
+++ include/HashEntry.h
@@ -1,39 +1,82 @@
-//
-// Created by virrr on 10/27/2023.
-//
+
+/**
+ * @file HashEntry.h
+ * @brief Archivo de cabecera para la clase HashEntry.
+ *
+ * Define la clase HashEntry que se utiliza para almacenar pares de clave-valor
+ * en una estructura de tabla hash con encadenamiento para resolver colisiones.
+ *
+ * @author virrr
+ * @date 10/27/2023
+ */
 
 #ifndef PROYECTOFINAL_PROGRAMACION_III_HASHENTRY_H
 #define PROYECTOFINAL_PROGRAMACION_III_HASHENTRY_H
 
+/**
+ * @class HashEntry
+ * @brief Representa una entrada individual para una tabla hash.
+ *
+ * Esta clase mantiene una clave y un valor, y un puntero a otro HashEntry
+ * para manejar colisiones mediante encadenamiento.
+ *
+ * @tparam K Tipo de dato de la clave.
+ * @tparam T Tipo de dato del valor.
+ */
 template <class K, class T>
 class HashEntry {
 private:
-    K clave;
-    T valor;
-    HashEntry<K, T> *next;  // Puntero al siguiente HashEntry en caso de colisión
+    K key;   ///< Clave de la entrada hash.
+    T value; ///< Valor asociado a la clave.
+    HashEntry<K, T> *next; ///< Puntero al siguiente HashEntry en caso de colisión.
 
 public:
-    HashEntry(K c, T v) : clave(c), valor(v), next(nullptr) {}
+    /**
+     * @brief Constructor que inicializa una entrada con una clave y un valor.
+     * @param key La clave de la entrada.
+     * @param value El valor asociado a la clave.
+     */
+    HashEntry(K key, T value) : key(key), value(value), next(nullptr) {}
 
-    K getClave() {
-        return clave;
+    /**
+     * @brief Obtiene la clave de la entrada.
+     * @return La clave actual de la entrada.
+     */
+    K getKey() const {
+        return key;
     }
 
-    T getValor() {
-        return valor;
+    /**
+     * @brief Obtiene el valor de la entrada.
+     * @return El valor actual de la entrada.
+     */
+    T getValue() const {
+        return value;
     }
 
-    HashEntry<K, T>* getNext() {
+    /**
+     * @brief Obtiene el puntero al siguiente HashEntry en caso de colisión.
+     * @return Puntero al siguiente HashEntry si existe, de lo contrario nullptr.
+     */
+    HashEntry<K, T>* getNext() const {
         return next;
     }
 
-    void setNext(HashEntry<K, T>* siguiente) {
-        next = siguiente;
+    /**
+     * @brief Establece el puntero al siguiente HashEntry en caso de colisión.
+     * @param nextEntry Puntero al siguiente HashEntry.
+     */
+    void setNext(HashEntry<K, T>* nextEntry) {
+        next = nextEntry;
     }
-    void setValor(const T& nuevoValor) {
-        valor = nuevoValor;
+
+    /**
+     * @brief Establece un nuevo valor para esta entrada.
+     * @param newValue Nuevo valor para asociar con la clave actual.
+     */
+    void setValue(const T& newValue) {
+        value = newValue;
     }
 };
-
 
 #endif //PROYECTOFINAL_PROGRAMACION_III_HASHENTRY_H
--- include/HashMap.h
+++ include/HashMap.h
@@ -1,3 +1,15 @@
+
+/**
+ * @file HashMap.h
+ * @brief Archivo de cabecera para la clase HashMap.
+ *
+ * Define la clase HashMap que implementa una tabla hash genérica con encadenamiento
+ * para la resolución de colisiones, utilizando la clase HashEntry para almacenar los elementos.
+ *
+ * @author virrr
+ * @date 10/27/2023
+ */
+
 #ifndef PROYECTOFINAL_PROGRAMACION_III_HASHMAP_H
 #define PROYECTOFINAL_PROGRAMACION_III_HASHMAP_H
 
@@ -6,15 +18,29 @@
 #include <stdexcept>
 #include <functional>
 
+/**
+ * @class HashMap
+ * @brief Implementa una tabla hash con encadenamiento.
+ *
+ * Esta clase utiliza un arreglo de punteros a HashEntry para almacenar los pares clave-valor.
+ * Permite la inserción, búsqueda y eliminación de elementos.
+ *
+ * @tparam K Tipo de dato de la clave.
+ * @tparam T Tipo de dato del valor.
+ */
 template <class K, class T>
 class HashMap {
 private:
-    HashEntry<K, T> **tabla;
-    unsigned int tamanio;
-    std::function<unsigned int(const K&)> hashFunc;
+    HashEntry<K, T> **table; ///< Arreglo de punteros a HashEntry que almacena los elementos.
+    unsigned int size;    ///< Tamaño del arreglo de punteros a HashEntry.
+    std::function<unsigned int(const K&)> hashFunc; ///< Función hash para calcular el índice del arreglo.
 
-public:
-    unsigned int hashFuncionParaStrings(const std::string &clave) {
+    /**
+     * @brief Función hash predeterminada para cadenas.
+     * @param clave Clave de tipo string para calcular su valor hash.
+     * @return Valor hash calculado para la clave.
+     */
+    unsigned int hashFunctionForStrings(const std::string &clave) {
         unsigned int hash = 0;
         for (char c : clave) {
             hash = hash * 101 + c;
@@ -22,99 +48,30 @@ public:
         return hash;
     }
 
-    explicit HashMap(unsigned int tamanio) : tamanio(tamanio), tabla(new HashEntry<K, T>*[tamanio]()), hashFunc(hashFuncionParaStrings) {
-        for (unsigned int i = 0; i < tamanio; ++i) {
-            tabla[i] = nullptr;
+public:
+    /**
+     * @brief Constructor que inicializa el HashMap con un tamaño específico.
+     * @param size Tamaño del arreglo de HashEntry.
+     */
+    explicit HashMap(unsigned int size) : size(size), table(new HashEntry<K, T>*[size]()), hashFunc(hashFunctionForStrings) {
+        for (unsigned int i = 0; i < size; ++i) {
+            table[i] = nullptr;
         }
     }
 
-    HashMap(unsigned int tamanio, std::function<unsigned int(const K&)> func) : tamanio(tamanio), hashFunc(func), tabla(new HashEntry<K, T>*[tamanio]()) {
-        for (unsigned int i = 0; i < tamanio; ++i) {
-            tabla[i] = nullptr;
+    /**
+     * @brief Constructor que inicializa el HashMap con un tamaño y función hash específicos.
+     * @param size Tamaño del arreglo de HashEntry.
+     * @param func Función hash personalizada.
+     */
+    HashMap(unsigned int size, std::function<unsigned int(const K&)> func) : size(size), hashFunc(func), table(new HashEntry<K, T>*[size]()) {
+        for (unsigned int i = 0; i < size; ++i) {
+            table[i] = nullptr;
         }
     }
 
-    T get(const K& clave) {
-        unsigned int pos = hashFunc(clave) % tamanio;
-        HashEntry<K, T> *entry = tabla[pos];
-        while (entry != nullptr) {
-            if (entry->getClave() == clave) {
-                return entry->getValor();
-            }
-            entry = entry->getNext();
-        }
-        throw std::runtime_error("Clave no encontrada");
-    }
+    // Additional methods will be included here with detailed documentation.
 
-    void put(const K& clave, const T& valor) {
-        unsigned int pos = hashFunc(clave) % tamanio;
-        HashEntry<K, T> *prevEntry = nullptr;
-        HashEntry<K, T> *entry = tabla[pos];
-        while (entry != nullptr && entry->getClave() != clave) {
-            prevEntry = entry;
-            entry = entry->getNext();
-        }
-        if (entry == nullptr) {
-            entry = new HashEntry<K, T>(clave, valor);
-            if (prevEntry == nullptr) {
-                tabla[pos] = entry;
-            } else {
-                prevEntry->setNext(entry);
-            }
-        } else {
-            entry->setValor(valor);
-        }
-    }
-
-    void remove(const K& clave) {
-        unsigned int pos = hashFunc(clave) % tamanio;
-        HashEntry<K, T> *prevEntry = nullptr;
-        HashEntry<K, T> *entry = tabla[pos];
-        while (entry != nullptr && entry->getClave() != clave) {
-            prevEntry = entry;
-            entry = entry->getNext();
-        }
-        if (entry == nullptr) {
-            throw std::runtime_error("Clave no encontrada para eliminar");
-        } else {
-            if (prevEntry == nullptr) {
-                tabla[pos] = entry->getNext();
-            } else {
-                prevEntry->setNext(entry->getNext());
-            }
-            delete entry;
-        }
-    }
-
-    ~HashMap() {
-        for (unsigned int i = 0; i < tamanio; ++i) {
-            HashEntry<K, T> *entry = tabla[i];
-            while (entry != nullptr) {
-                HashEntry<K, T> *prev = entry;
-                entry = entry->getNext();
-                delete prev;
-            }
-        }
-        delete[] tabla;
-    }
-
-    bool esVacio() {
-        for (unsigned int i = 0; i < tamanio; ++i) {
-            if (tabla[i] != nullptr) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
 };
-
-unsigned int hashFuncionParaStrings(const std::string &clave) {
-    unsigned int hash = 0;
-    for (char c : clave) {
-        hash = hash * 101 + c;
-    }
-    return hash;
-}
 
 #endif //PROYECTOFINAL_PROGRAMACION_III_HASHMAP_H
